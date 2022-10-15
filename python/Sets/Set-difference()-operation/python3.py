d8fa2f9386c3c3113c0bfce867762afe5c0ec8e0
PRIYANK GOSWAMI <109909710+CodeWithPriyank@users.noreply.github.com>
2022-10-16 01:50:20 +0530

    Create python3.py

---
+++ python/Sets/Set-difference()-operation/python3.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+eng_news = int(input())
+n = set(map(int,input().split()))
+french_news = int(input())
+b = set(map(int,input().split()))
+print(len(n.difference(b)))
